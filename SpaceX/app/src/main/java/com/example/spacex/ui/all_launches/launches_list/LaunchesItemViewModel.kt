d9ddf6dece874886afb825369f754222791072e1
Ben <tttw216@gmail.com>
2022-07-31 17:21:45 +0800

    [Feat] add list files

---
+++ SpaceX/app/src/main/java/com/example/spacex/ui/all_launches/launches_list/LaunchesItemViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.spacex.ui.all_launches.launches_list
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.spacex.ui.base.BindingHolder
+
+class LaunchesItemViewModel : RecyclerView.Adapter<BindingHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BindingHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: BindingHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
