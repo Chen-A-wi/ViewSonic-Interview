6135dff0692b97fd8f4dad3407e7b37b90190734
Ben <tttw216@gmail.com>
2022-07-31 12:54:21 +0800

    [Feat] check internet

--- SpaceX/app/src/main/java/com/example/spacex/ui/base/BaseFragment.kt
+++ SpaceX/app/src/main/java/com/example/spacex/ui/base/BaseFragment.kt
@@ -1,14 +1,19 @@
 package com.example.spacex.ui.base
 
 import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.util.Log
 import android.view.View
 import androidx.annotation.StringRes
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import com.afollestad.materialdialogs.MaterialDialog
 import com.example.spacex.R
 import com.example.spacex.databinding.IncludeAppBarBinding
 import com.example.spacex.ui.MainActivity
+import kotlin.system.exitProcess
 
 abstract class BaseFragment : Fragment() {
     lateinit var act: MainActivity
@@ -25,7 +30,6 @@ abstract class BaseFragment : Fragment() {
         @StringRes appbarTitleResId: Int = R.string.empty,
         isShowLeftButton: Boolean = false,
     ) {
-
         fun setupButton(imageButton: View, isShowButton: Boolean) {
             if (isShowButton) {
                 imageButton.isVisible = true
@@ -42,5 +46,54 @@ abstract class BaseFragment : Fragment() {
             tvTitle.text = getString(appbarTitleResId)
             setupButton(imgLeft, isShowLeftButton)
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!checkInternet()) {
+            showPleaseInternetDialog()
+        }
+    }
+
+    private fun checkInternet(): Boolean {
+        val connectivityManager =
+            ctx.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val capabilities =
+            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        if (capabilities != null) {
+            when {
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
+                    Log.i("[Internet]", "NetworkCapabilities.TRANSPORT_CELLULAR")
+                    return true
+                }
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
+                    Log.i("[Internet]", "NetworkCapabilities.TRANSPORT_WIFI")
+                    return true
+                }
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> {
+                    Log.i("[Internet]", "NetworkCapabilities.TRANSPORT_ETHERNET")
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+    private fun showPleaseInternetDialog() {
+        MaterialDialog(ctx).show {
+            cancelable(true)
+            message(text = context.getString(R.string.alert_check_internet_msg))
+        }.positiveButton(res = R.string.btn_confirm) {
+            if (!checkInternet()) {
+                closeApp()
+            }
+        }.negativeButton(res = R.string.btn_cancel) {
+            closeApp()
+        }
+    }
+
+    private fun closeApp() {
+        act.finish()
+        exitProcess(0)
     }
 }
