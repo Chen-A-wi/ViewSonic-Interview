d48f9f9ec48550cb1e23f93f0f9fb1329ba21c00
Ben <tttw216@gmail.com>
2022-07-31 17:59:47 +0800

    [Fix] fix adapter

--- SpaceX/app/src/main/java/com/example/spacex/ui/all_launches/launches_list/LaunchesListAdapter.kt
+++ SpaceX/app/src/main/java/com/example/spacex/ui/all_launches/launches_list/LaunchesListAdapter.kt
@@ -1,4 +1,19 @@
 package com.example.spacex.ui.all_launches.launches_list
 
-class LaunchesListAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.spacex.ui.base.BindingHolder
+
+class LaunchesListAdapter : RecyclerView.Adapter<BindingHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BindingHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: BindingHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
